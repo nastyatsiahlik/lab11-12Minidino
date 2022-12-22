efa3e97a59d4c55dfdc162966269b279ea5d3aff
nastyatsiahlik <tyahliki.n@gmail.com>
2022-12-22 15:36:25 +0300

    fix code

--- src/test/java/service/ProductCreator.java
+++ src/test/java/service/ProductCreator.java
@@ -5,11 +5,11 @@ import model.Product;
 public class ProductCreator {
 
     //public static  String TESTDATA_TO_CART_PRODUCT_NAME =  "testdata.product.nameForCart";
-    public static  String TESTDATA_TO_CART_PRODUCT_PRICE =  "testdata.product.priceForCart";
+    //public static  String TESTDATA_TO_CART_PRODUCT_PRICE =  "testdata.product.priceForCart";
     //public static  String TESTDATA_TO_CART_PRODUCT_URL =  "testdata.product.urlForCart";
 
     public static  String TESTDATA_TO_CART_PRODUCT_NAME = "Комбинезон для сна принт лыжник";
-   // public static  String TESTDATA_TO_CART_PRODUCT_PRICE = "1890";
+    public static  String TESTDATA_TO_CART_PRODUCT_PRICE = "1890";
     public static  String TESTDATA_TO_CART_PRODUCT_URL = "https://минидино.рф/catalog/odezhda/kombinezon_dlya_sna_print_lyzhnik/?offer_id=55555";
 
     public static  String TESTDATA_TO_HIGHLIGHTS_PRODUCT_NAME = "Юбка Школа серая клетка";
